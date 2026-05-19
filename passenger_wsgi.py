2240b328865effd095be95281d70d317529f60ab
dallah-254 <dallaherick0@gmail.com>
2026-05-19 16:38:25 +0300

    refactor: restructure repo to match server directory layout

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-
-sys.path.insert(0, os.path.dirname(__file__))
+# Point to the fagierrandsbackup folder
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'fagierrandsbackup'))
 
 from fagierrandsbackup.wsgi import application
